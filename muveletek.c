77d17022933d421b574dfa622c8d384e9eb45f1a
Hespi <sardi.andras@gmail.com>
2020-10-06 18:07:29 +0200

    Add files via upload

---
+++ muveletek.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int a = 30, b = 10;
+	printf("a = %d, b = %d\n", a, b);
+	printf("a + b = %d\n", a+b);
+	printf("a - b = %d\n", a-b);
+	printf("a / b = %d\n", a/b);
+	printf("a * b = %d\n", a*b);
+
+	printf("a %% b = %d\n", a % b);
+
+	return 0;
+}
